21ab0392700ec6b327c267118e4243b1850e3e0e
Rohan <83257221+Rohan-tech11@users.noreply.github.com>
2021-06-22 09:27:30 +0530

    Update practise.java

--- practise.java
+++ practise.java
@@ -1,1 +1,15 @@
 System.out.println("hi this is demo");
+class A
+{
+int a=123;
+double b=1.23l;
+
+  public double add()
+  {
+    return a+b;
+  }
+  public static main(String[] args)
+  {
+    A a1=new A();
+  }
+}
